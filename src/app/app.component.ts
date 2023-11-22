ebce7237e8bc1b8c7351a171c91326b538cbf831
Citrom333 <citrom333@gmail.com>
2023-11-22 08:47:59 +0100

    feat: add toggle and fade to AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { trigger, state, transition, style, animate } from '@angular/animations';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, RouterOutlet
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('fade', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate(1000, style({ opacity: 1 }))
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate(1000, style({ opacity: 0 }))
+      ]),
+    ]),
+
+  ]
 })
+
 export class AppComponent {
   title = 'portfoliopageapp';
+  showLooseText: boolean = false;
+
+  toggleText() {
+    this.showLooseText = !this.showLooseText;
+  }
+  showLooseText2: boolean = false;
+
+  toggleText2() {
+    this.showLooseText2 = !this.showLooseText2;
+  }
 }
