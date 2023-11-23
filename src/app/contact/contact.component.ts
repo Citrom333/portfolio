8d703b8eaca808666b42b677886f37e1f88d8899
Citrom333 <citrom333@gmail.com>
2023-11-23 20:35:44 +0100

    feat: add email sender to contact component

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
+  constructor(private http: HttpClient) {
 
+  }
+  onEmailSend(email: { senderMail: string, subject: string, message: string }) {
+    console.log(email);
+    const requestBody: any = { subject: email.subject, text: `${email.message} FROM ${email.senderMail}` };
+    console.log(requestBody);
+    this.http.post("http://localhost:3500/send-email", requestBody)
+      .subscribe({
+        next: response => {
+          console.log('Sikeres POST kérés:', response);
+          // Itt további logikát vagy kezelést tehetsz meg a válasz alapján
+        },
+        error: error => {
+          console.error('Hiba a POST kérés során:', error);
+          // Itt további hiba kezelést tehetsz meg
+        }
+      });
+  }
 }
