dd86d7cc64a67df5786143a2abde2587d06b22e4
Citrom333 <citrom333@gmail.com>
2023-11-23 20:35:07 +0100

    feat: add email sending to backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const cors = require('cors');
+const { default: test } = require('node:test');
 const app = express();
-
+const sgMail = require('@sendgrid/mail')
+require('dotenv').config();
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+app.use(cors());
+app.use(express.json());
 // Define route for file download
 app.get('/download', (req, res) => {
 
@@ -12,6 +18,29 @@ app.get('/download', (req, res) => {
         }
     });
 });
+app.post('/send-email', (req, res) => {
+    console.log(sgMail);
+    const { subject, text } = req.body;
+    const msg = {
+        to: "citrom333@gmail.com",
+        from: 'citrom333@gmail.com',
+        subject: subject,
+        text: text,
+        html: `<strong>${text}</strong>`,
+    };
+
+    sgMail
+        .send(msg)
+        .then(() => {
+            console.log('Email sent');
+            res.status(200).json({ success: true, message: 'Email sent successfully.' });
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).json({ success: false, message: 'Failed to send email.' });
+        });
+});
+
 // Serve your Angular app
 app.use(express.static('../portfoliopageapp/dist/portfoliopageapp'));
 
